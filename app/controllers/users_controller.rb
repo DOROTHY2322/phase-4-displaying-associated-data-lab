a91653e476756bf0a25f71ef74f420c6ed673772
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-15 20:58:36 +0300

    displaying associated data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
+# app/controllers/users_controller.rb
 class UsersController < ApplicationController
-end
+    def show
+      user = User.find(params[:id])
+      user_data = {
+        id: user.id,
+        username: user.username,
+        city: user.city,
+        items: user.items.map do |item|
+          {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            price: item.price
+          }
+        end
+      }
+      render json: user_data
+    end
+  end
+  
